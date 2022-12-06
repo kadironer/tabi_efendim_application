5bcd6ec560674508164ce583e6a295b458d538c2
kadironer <kadironer46500@gmail.com>
2022-12-06 21:36:35 +0300

    Splash screen completed

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'home_page.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 1), () async {
+    Future.delayed(const Duration(seconds: 1, milliseconds: 30), () async {
       Navigator.pushReplacement(
           context, new MaterialPageRoute(builder: (context) => new HomePage()));
     });
@@ -22,12 +22,35 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
+      backgroundColor: Colors.red,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Tabi Efendim"),
+            SizedBox(
+              width: size.width * 0.6,
+              height: size.height * 0.6,
+              child: Card(
+                color: Colors.white,
+                shape: CircleBorder(),
+                shadowColor: Colors.grey,
+                elevation: 5,
+                child: Center(
+                  child: Text(
+                    "Tabi Efendim",
+                    style: GoogleFonts.poppins(
+                      fontSize: 25,
+                      letterSpacing: 1.5,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
